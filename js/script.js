315731ed04ae9f42cedc01a35b3abd3ec69c5207
Andrea <andrea3aprile99@gmail.com>
2021-03-29 15:14:06 +0200

    whatsapp-chats

--- js/script.js
+++ js/script.js
@@ -2,29 +2,7 @@
 // el: '#root',
 //
 // data: {
-//   items: [
-//     {
-//        img: 'img/avatar_2.jpg',
-//        text: 'Luigi',
-//        message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-//
-//
-//       },
-//       {
-//         img: 'img/avatar_7.jpg',
-//           text: 'Franco',
-//          message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-//
-//
-//          },
-//          {
-//            img: 'img/avatar_4.jpg',
-//           text: 'Davide',
-//          message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-//
-//
-//        }
-//   ]
+
 // }
 //
 //
@@ -32,16 +10,53 @@
 new Vue({
   el: '#root',
   data: {
-    object: {
+    object: [{
       img: 'img/avatar_2.jpg',
       text: 'Luigi',
       message: 'Ciao Franco come va? ' + new Date().toLocaleString()
     },
-  //   {
-  //     img: 'img/avatar_4.jpg',
-  //     text: 'Davide',
-  //     message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-  // }
+    {
+      img: 'img/avatar_7.jpg',
+        text: 'Franco',
+       message: 'Ciao Franco come va? ' + new Date().toLocaleString()
 
+
+       },
+       {
+         img: 'img/avatar_4.jpg',
+        text: 'Davide',
+       message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+
+
+     }
+   ]
   }
 })
+// new Vue({
+//   el: '#root',
+// object: [
+//     {
+       // img: 'img/avatar_2.jpg',
+       // text: 'Luigi',
+       // message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+//
+//
+//       },
+      // {
+      //   img: 'img/avatar_7.jpg',
+      //     text: 'Franco',
+      //    message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+      //
+      //
+      //    },
+      //    {
+      //      img: 'img/avatar_4.jpg',
+      //     text: 'Davide',
+      //    message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+      //
+      //
+      //  }
+//   ]
+//
+//
+// });
