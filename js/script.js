8f03171662447e75d06c63ff1debf93b80246f2b
Andrea <andrea3aprile99@gmail.com>
2021-03-29 23:20:12 +0200

    stampa messaggi

--- js/script.js
+++ js/script.js
@@ -5,72 +5,84 @@ new Vue({
     object: [{
       img: 'img/avatar_2.jpg',
       text: 'Luigi',
-      message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+      message:[ 'Ciao Franco come va? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+      risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
     },
     {
       img: 'img/avatar_3.jpg',
         text: 'Franco',
-       message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+       message: ['Domani Calcietto, 19? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+      risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
        },
        {
          img: 'img/avatar_4.jpg',
         text: 'Davide',
-       message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+       message: ['Senza casa, ne tetto ! ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+      risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
 
      },
      {
        img: 'img/avatar_5.jpg',
        text: 'Don.pippo',
-       message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+       message:[ 'Ciao Franco come va? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+      risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
      },
      {
        img: 'img/avatar_7.jpg',
          text: 'Pallina',
-        message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+        message: ['Domani Calcietto, 19? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+        risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
         },
         {
           img: 'img/avatar_6.jpg',
          text: 'Robin',
-        message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+        message: ['Senza casa, ne tetto ! ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+        risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
 
       },
       {
         img: 'img/avatar_2.jpg',
         text: 'Luigi',
-        message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+        message:[ 'Ciao Franco come va? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+        risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
       },
       {
         img: 'img/avatar_3.jpg',
           text: 'Franco',
-         message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+         message: ['Domani Calcietto, 19? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+        risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
          },
          {
            img: 'img/avatar_4.jpg',
           text: 'Davide',
-         message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+         message: ['Senza casa, ne tetto ! ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+        risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
 
        },
        {
          img: 'img/avatar_5.jpg',
          text: 'Don.pippo',
-         message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+         message:[ 'Ciao Franco come va? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+        risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
        },
        {
          img: 'img/avatar_7.jpg',
            text: 'Pallina',
-          message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+          message: ['Domani Calcietto, 19? ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+          risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
           },
           {
             img: 'img/avatar_6.jpg',
            text: 'Robin',
-          message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+          message: ['Senza casa, ne tetto ! ' ,"Ti va di andare in piscina","il mio colore preferito e' il giallo","Sono andato alle Maldive","Anche se non ci credi andro' a prendere il sushi","il ninja dietro alle tue spalle","sisi 2+2 fa 4!","se ti chiamo mi rispondi?","la mia ragazza e' andata in biblioteca","tre giorni dopo la chiamai","dimmi chi ha inventato il natale","tre giorni dopo bevve una birra","non e' assolutamente risorto il sabato"],
+          risposte: ['secondo me arriva domani ' ,"come vorrei che Stan fosse qui","e' meglio vedersi vestiti di nero che di rosso","ti va se affittiamo una barca?","oggi non posso vado in palestra","a che ora arriva la pizza","ti richiamo domani","quello che pensi e' naturale","il conto lo paghi tu","today is the day","natale a cancun","la mia macchina preferita e' la toyota","Holly e' venuta a cena con me!"]
 
 
         }
