8bcd7ed6e0aeee94ffc703a775673a2a8e93262e
Andrea <andrea3aprile99@gmail.com>
2021-03-29 17:44:37 +0200

    scroll chat

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-// var root = new Vue({
-// el: '#root',
-//
-// data: {
 
-// }
-//
-//
-// })
 new Vue({
   el: '#root',
   data: {
@@ -16,47 +8,78 @@ new Vue({
       message: 'Ciao Franco come va? ' + new Date().toLocaleString()
     },
     {
-      img: 'img/avatar_7.jpg',
+      img: 'img/avatar_3.jpg',
         text: 'Franco',
-       message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-
+       message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
 
        },
        {
          img: 'img/avatar_4.jpg',
         text: 'Davide',
+       message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+
+
+     },
+     {
+       img: 'img/avatar_5.jpg',
+       text: 'Don.pippo',
        message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+     },
+     {
+       img: 'img/avatar_7.jpg',
+         text: 'Pallina',
+        message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+
+        },
+        {
+          img: 'img/avatar_6.jpg',
+         text: 'Robin',
+        message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
 
 
-     }
-   ]
-  }
+      },
+      {
+        img: 'img/avatar_2.jpg',
+        text: 'Luigi',
+        message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+      },
+      {
+        img: 'img/avatar_3.jpg',
+          text: 'Franco',
+         message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+
+         },
+         {
+           img: 'img/avatar_4.jpg',
+          text: 'Davide',
+         message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+
+
+       },
+       {
+         img: 'img/avatar_5.jpg',
+         text: 'Don.pippo',
+         message: 'Ciao Franco come va? ' + new Date().toLocaleString()
+       },
+       {
+         img: 'img/avatar_7.jpg',
+           text: 'Pallina',
+          message: 'Domani Calcietto, 19? ' + new Date().toLocaleString()
+
+          },
+          {
+            img: 'img/avatar_6.jpg',
+           text: 'Robin',
+          message: 'Senza casa, ne tetto ! ' + new Date().toLocaleString()
+
+
+        }
+   ],
+   currentUser:0
+ },
+ methods:{
+   prova:function(index){
+     this.currentUser=index;
+   }
+ }
 })
-// new Vue({
-//   el: '#root',
-// object: [
-//     {
-       // img: 'img/avatar_2.jpg',
-       // text: 'Luigi',
-       // message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-//
-//
-//       },
-      // {
-      //   img: 'img/avatar_7.jpg',
-      //     text: 'Franco',
-      //    message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-      //
-      //
-      //    },
-      //    {
-      //      img: 'img/avatar_4.jpg',
-      //     text: 'Davide',
-      //    message: 'Ciao Franco come va? ' + new Date().toLocaleString()
-      //
-      //
-      //  }
-//   ]
-//
-//
-// });
